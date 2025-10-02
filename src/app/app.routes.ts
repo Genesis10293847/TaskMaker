cc1a82074de8c4b663c7132d717596678778435b
Genesis Monje <monje_g@ca01.ub.lan>
2025-10-02 13:03:29 +0800

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
+import { Component } from '@angular/core';
+import { Home } from './home/home';
+import { Summary } from './summary/summary';
 import { Routes } from '@angular/router';
+import { TaskForm } from './task-form/task-form';
+import { TaskList } from './task-list/task-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: Home},
+  {path: 'task-form', component: TaskForm},
+  {path: 'task-list', component: TaskList},
+  {path: 'summary', component: Summary}
+];
